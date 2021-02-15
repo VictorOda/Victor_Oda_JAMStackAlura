b00351f8e8dad2da52aee5665f792f8b14e0a36a
Victor Oda <victor.ra.oda@gmail.com>
2021-02-15 17:58:57 -0300

    Add Fira Sans font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Fira Sans', sans-serif;
+    // Deixa branco no começo
+    color: #292F35;
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `;
 
@@ -19,6 +36,11 @@ const theme = {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>&lt;oda /&gt;</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@200;400;700;900&family=Fira+Sans:wght@400&display=swap" rel="stylesheet" />
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
