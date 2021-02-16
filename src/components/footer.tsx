57ca314c2b549a4e88915cab611d58f3722c6acf
Victor Oda <victor.ra.oda@gmail.com>
2021-02-16 11:17:20 -0300

    Add footer layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,15 +2,40 @@ import React from 'react';
 import styled from 'styled-components';
 
 const WrapperFooter = styled.div`
-  background-color: #141517;
+  background-color: ${({ theme }) => theme.colors.header};
   width: 100vw;
   height: 60px;
+`;
+
+const FooterBackground = styled.footer`
+  background-color: ${({ theme }) => theme.colors.primary};
+  width: 104px;
+  height: 60px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SocialButton = styled.img`
+  margin: 6px;
+  height: 32px;
+  width: 32px;
 `;
 
 export default function Footer() {
   return (
     <div>
-      <WrapperFooter />
+      <WrapperFooter>
+        <FooterBackground>
+          <a href="https://github.com/VictorOda">
+            <SocialButton src="./buttons/github.png" alt="githubIcon" />
+          </a>
+          <a href="https://www.linkedin.com/in/victor-oda-65b782106/">
+            <SocialButton src="./buttons/linkedin.png" alt="linkedinIcon" />
+          </a>
+        </FooterBackground>
+      </WrapperFooter>
     </div>
   );
 }
