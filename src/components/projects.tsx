14ad925b7c0ece21d6e7cef68e8c816fe1b0f9b6
Victor Oda <victor.ra.oda@gmail.com>
2021-02-17 11:22:19 -0300

    Add breakpoints for projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import breakpointsMedia from '../theme/utils/breakpointsMedia';
 
 const WrapperProjects = styled.div`
-  background-color: #FFF;
+  background-color: ${({ theme }) => theme.colors.lightTheme.background};
   width: 100%;
   min-height: 300px;
-  padding: 18px
+  ${breakpointsMedia({
+    xs: css`
+      padding: 18px;
+    `,
+    md: css`
+      padding: 36px;
+    `,
+  })}
 `;
 
-const Title = styled.span`
-  font-size: 36px;
-  color: ${({ theme }) => theme.colors.header};
+const SectionTitle = styled.span`
+  color: ${({ theme }) => theme.colors.lightTheme.mainText};
+  ${breakpointsMedia({
+    xs: css`
+      font-size: ${({ theme }) => theme.typographyVariants.sectionTitleXS.fontSize};
+      font-weight: ${({ theme }) => theme.typographyVariants.sectionTitleXS.fontWeight};
+    `,
+    md: css`
+      font-size: ${({ theme }) => theme.typographyVariants.sectionTitle.fontSize};
+      font-weight: ${({ theme }) => theme.typographyVariants.sectionTitle.fontWeight};
+    `,
+  })}
 `;
 
 const FeaturedProject = styled.div`
-  width: 80%;
-  margin: 18px auto;
-  border: 2px solid ${({ theme }) => theme.colors.contrastBackground};
+  
+  border: 2px solid ${({ theme }) => theme.colors.lightTheme.secondaryText};
   display: flex;
   flex-direction: row;
+  ${breakpointsMedia({
+    xs: css`
+      margin: 18px auto;
+      width: 500px;
+    `,
+    md: css`
+      margin: 36px auto;
+      min-width: 500px;
+      width: 80%;
+    `,
+  })}
 `;
 
 const Screenshot = styled.img`
@@ -27,7 +54,6 @@ const Screenshot = styled.img`
 `;
 
 const Description = styled.div`
-  background-color: ${({ theme }) => theme.colors.contrastBackground};
   flex-direction: column;
   padding: 18px;
 `;
@@ -35,18 +61,18 @@ const Description = styled.div`
 const ProjectTitle = styled.span`
   font-weight:900;
   font-size: 36px;
-  color: ${({ theme }) => theme.colors.text};
+  color: ${({ theme }) => theme.colors.lightTheme.highlight};
 `;
 
 const ProjectDescription = styled.p`
   font-size: 18px;
-  color: ${({ theme }) => theme.colors.text};
+  color: ${({ theme }) => theme.colors.lightTheme.mainText};
 `;
 
 export default function Projects() {
   return (
     <WrapperProjects>
-      <Title>/projects</Title>
+      <SectionTitle>/projects</SectionTitle>
       <FeaturedProject>
         <Screenshot src="./images/quiz-screenshot.jpg" alt="quiz" />
         <Description>
