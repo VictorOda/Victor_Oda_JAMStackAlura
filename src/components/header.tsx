9b72848ccfb7a95d1e7af9ba8886db0f62ca85f5
Victor Oda <victor.ra.oda@gmail.com>
2021-02-16 21:50:11 -0300

    Add basic heade layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { ThemeConsumer } from 'styled-components';
 
 const WrapperHeader = styled.section`
-  width: 100vw;
+  width: 100%;
   height: 80px;
 `;
 
 const Navbar = styled.nav<{isSticky: boolean}>`
   background-color: ${({ theme }) => theme.colors.header};
   border-top: 2px solid ${({ theme }) => theme.colors.primary};
-  width: 100vw;
+  width: 100%;
   height: 80px;
-  
+
   ${(prop) => {
     if (prop.isSticky) {
       return `
@@ -22,6 +22,39 @@ const Navbar = styled.nav<{isSticky: boolean}>`
     }
     return '';
   }}
+`;
+
+const Logo = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary};
+  margin: 0 10%;
+  width: 136px;
+  height: 100%;
+  border: none;
+`;
+
+const LogoText = styled.span`
+  font-size: 36px;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+const WrapperLinks = styled.div`
+  width: 50%;
+  height: 100%;
+  float: right;
+  display: flex;
+  flex-direction: row-reverse;
+`;
+
+const LinkButton = styled.button`
+  border: none;
+  width: 100px;
+  margin: 0 18px;
+  background-color: transparent;
+`;
+
+const LinkText = styled.span`
+  font-size: 28px;
+  color: ${({ theme }) => theme.colors.text};
 `;
 
 export default function Header() {
@@ -45,7 +78,14 @@ export default function Header() {
   return (
     <WrapperHeader id="navbar-container">
       <Navbar isSticky={isSticky}>
-        Test
+        <Logo>
+          <LogoText>&lt;oda/&gt;</LogoText>
+        </Logo>
+        <WrapperLinks>
+          <LinkButton><LinkText>/contact</LinkText></LinkButton>
+          <LinkButton><LinkText>/about</LinkText></LinkButton>
+          <LinkButton><LinkText>/projects</LinkText></LinkButton>
+        </WrapperLinks>
       </Navbar>
     </WrapperHeader>
   );
