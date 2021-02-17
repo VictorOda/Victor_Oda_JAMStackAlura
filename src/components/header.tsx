ca9e2f65380abc48cdb9da5eece210a8ab0cb866
Victor Oda <victor.ra.oda@gmail.com>
2021-02-17 08:59:13 -0300

    Remove unused import

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import styled, { ThemeConsumer } from 'styled-components';
+import styled from 'styled-components';
 
 const WrapperHeader = styled.section`
   width: 100%;
