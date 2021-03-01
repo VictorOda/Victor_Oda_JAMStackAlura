373f23931bfba93761dddb126ff95c81cf0cdf20
Victor Oda <victor.ra.oda@gmail.com>
2021-02-28 22:02:04 -0300

    Add responsiviness

--- src/components/common/forms/formContact.tsx
+++ src/components/common/forms/formContact.tsx
@@ -1,28 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
+import { css } from 'styled-components';
+import breakpointsMedia from '../../../theme/utils/breakpointsMedia';
 
-const WrapperForm = styled.div`
+const WrapperForm = styled.form`
   display: flex;
-  justify-content: center;
-  align-content: end;
-  width: 40%;
+  flex-direction: column;
+  padding: 8px 32px;
   margin: auto;
   background: #fff;
+
+  ${breakpointsMedia({
+    xs: css`
+      width: 100%;
+    `,
+    md: css`
+        max-width: 768px;
+        width: 60%;
+    `,
+  })}
+`;
+
+const TextInput = styled.input`
+  width: 90%;
+  margin: auto;
+  height: 32px;
+`;
+
+const Title = styled.h1`
+  margin: auto;
+  padding: 8px 0;
+`;
+
+const OptionText = styled.h3`
+  padding-left: 5%;
+`;
+
+const FormButton = styled.h2`
+  margin: auto;
+  padding: 16px;
 `;
 
 export default function FormContact() {
   return (
     <WrapperForm>
-      <form>
-        <h1>ENVIE SUA MENSAGEM</h1>
-        <h3>Seu nome</h3>
-        <input type="text" id="name" />
-        <h3>Seu email</h3>
-        <input type="text" id="email" />
-        <h3>Sua mensagem</h3>
-        <input type="text" id="message" />
-        <h4>ENVIAR</h4>
-      </form>
+      <Title>ENVIE SUA MENSAGEM</Title>
+      <OptionText>Seu nome</OptionText>
+      <TextInput type="text" id="name" />
+      <OptionText>Seu email</OptionText>
+      <TextInput type="text" id="email" />
+      <OptionText>Sua mensagem</OptionText>
+      <TextInput type="text" id="message" />
+      <FormButton>ENVIAR</FormButton>
     </WrapperForm>
   );
 }
